cf6784d22c1fc984549af1e5ca498eb11f0b3b2a
thanglh <thanglh.199x@gmail.com>
2023-06-12 17:43:08 +0700

    12/06

---
+++ src/main/java/springsecurity/util/Mapper.java
@@ -0,0 +1,19 @@
+package springsecurity.util;
+
+import springsecurity.model.RegisterStudent;
+import springsecurity.model.Student;
+
+public class Mapper {
+    //chuyển từ dto registerStudent thành entity student
+    public static Student studentRegToStudent(RegisterStudent registerStudent){
+        Student student = new Student();
+            student.setName(registerStudent.getName());
+            student.setPassword(registerStudent.getPassword());
+            student.setFullname(registerStudent.getFullname());
+            student.setAge(registerStudent.getAge());
+            student.setEmail(registerStudent.getEmail());
+            student.setRole(registerStudent.getRole());
+            student.setActive(registerStudent.isActive());
+            return student;
+    }
+}
